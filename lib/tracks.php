556622811d6c7f1c863ab03f9b6f42e9a5d6db6f
Steffen Lorenz <lupin3000@MacBookPro.local>
2019-01-20 10:24:43 +0100

    add track duration

--- lib/tracks.php
+++ lib/tracks.php
@@ -1,10 +1,23 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+include './class/mp3file.class.php';
 
 $domain = $_SERVER['REQUEST_SCHEME'] . '://'. $_SERVER['HTTP_HOST'];
-$tracks_array = json_encode(array_map('basename', glob("mp3/*.mp3")));
-$tracks = array('url' => $domain, 'tracks' => $tracks_array);
+$folder_tracks = array_map('basename', glob("mp3/*.mp3"));
+$track_list = array();
 
+foreach ($folder_tracks as $value) {
+  $track = "./mp3/" . $value;
+  $mp3file = new MP3File($track);
+  $duration2 = $mp3file->getDuration();
+  $track_list[] = array(
+    'filename' => $value,
+    'duration' => MP3File::formatTime($duration2)
+  );
+}
+
+$response = array('url' => $domain . '/mp3/', 'tracks' => $track_list);
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 http_response_code(200);
-echo json_encode($tracks);
+echo json_encode($response);
